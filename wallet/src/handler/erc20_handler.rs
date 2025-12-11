3bece9beee7064350fdc923b14694be33149c777
brunowang <brunowang525@gmail.com>
2025-12-12 00:48:23 +0800

    fixed: erc20 getBalance interface

--- wallet/src/handler/erc20_handler.rs
+++ wallet/src/handler/erc20_handler.rs
@@ -6,21 +6,27 @@ use axum::extract::{Query, State};
 use axum::response::IntoResponse;
 use serde_json::json;
 use std::sync::Arc;
+use serde::Deserialize;
 
 pub struct ERC20Handler;
+
+#[derive(Deserialize)]
+pub struct ERC20BalanceRequest {
+    pub address: String,
+    pub contract_address: String,
+}
 
 impl ERC20Handler {
     pub async fn get_balance(
         State(app_state): State<Arc<AppState>>,
-        Query(address): Query<String>,
-        Query(contract_address): Query<String>,
+        Query(req): Query<ERC20BalanceRequest>,
     ) -> Result<impl IntoResponse, AppError> {
         let response = json!({
             "status":200,
             "message":"success",
             "data":{
                 "balance": ERC20Service::new(&app_state.eth)?
-                .get_balance(&address, &contract_address).await?
+                .get_balance(&req.address, &req.contract_address).await?
             }
         });
         Ok(Json(response))
--- wallet/src/router.rs
+++ wallet/src/router.rs
@@ -4,6 +4,7 @@ use crate::handler::wallet_handler::WalletHandler;
 use crate::model::app_model::AppState;
 use axum::{routing::get, routing::post, Router};
 use std::sync::Arc;
+use crate::handler::erc20_handler::ERC20Handler;
 
 pub fn create_route(app_state: Arc<AppState>) -> Router {
     let router = Router::new()
@@ -14,6 +15,7 @@ pub fn create_route(app_state: Arc<AppState>) -> Router {
         .route("/wallet/balance/{address}", get(WalletHandler::get_balance))
         .route("/wallet/transaction/{tx_hash}", get(WalletHandler::get_transaction))
         .route("/wallet/send", post(WalletHandler::send_transaction))
+        .route("/erc20/balance", get(ERC20Handler::get_balance))
         .with_state(app_state.clone());
     router
 }
